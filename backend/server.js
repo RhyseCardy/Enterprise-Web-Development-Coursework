1930e94dba1f7fc2a105263b0472775902954f53
RhyseCardy <91136666+RhyseCardy@users.noreply.github.com>
2023-03-21 22:07:46 +0000

    starter code for server setup and index web page

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,60 @@
 //
 //TEST SERVER
 // 
-const express = require('express')
-const app = express()
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser')
+const app = express();
+const {name} = require("ejs")
 const port = 3000
+const req = require('express/lib/request');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+//Create static folder
+app.use(express.static(path.join(__dirname, '/../frontend')));
+//setting up body parser
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//Setup ejs
+app.set('views', path.join(__dirname, '/../frontend/views'))
+app.set('view engine', 'ejs');
+
+app.engine('ejs', require('ejs').__express);
+
+
+//Setup the session
+app.use(session({secret: 'QuoteCreatorSecret', resave: false, saveUninitialized: false}));
+
+//The route for the home page (default)
+app.get('/', function(req, res){
+  res.render('pages/index')
+});
+
+//Also the route for the home page
+app.get('/home', function(req, res){
+  res.render('pages/index')
+});
+
+//The route for the login page
+app.get('/login', function(req, res){
+  error = false;
+  res.render('pages/login')
+});
+
+//The route for the quotes page 
+//WILL NEED ADDITIONAL WORK
+app.get('/quotes', function(req, res){
+  res.render('pages/list')
+});
+
+//Error response page
+app.use(function (req, res, next) {
+    res.send("this page does not exist")
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
